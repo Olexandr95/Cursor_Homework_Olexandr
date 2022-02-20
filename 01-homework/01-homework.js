f695d1f906662133e7c5a31b0f4323bc5c0fdf91
unknown <alexandr.ruschak95@gmail.com>
2022-02-20 14:43:39 +0200

    Fixed litle bag

--- 01-homework/01-homework.js
+++ 01-homework/01-homework.js
@@ -5,12 +5,12 @@ let pairNum;
 
 do {
 	min = +prompt('Від якого числа почнемо додавати?');
-} while (min % 1 != 0)
+} while (min % 1 != 0 )
 do {
 	max = +prompt('До якого числа будемо додавати?');
 } while (max % 1 != 0)
 pairNum = confirm("Пропускаємо парні числа?");
-
+if (max > min){
 for (i = min; i <= max; i++){
 	if (pairNum === true) {
             if (i % 2) {
@@ -20,6 +20,17 @@ for (i = min; i <= max; i++){
             sum += i
         }
     }
+} else{
+	for (i = max; i <= min; i++){
+	if (pairNum === true) {
+            if (i % 2) {
+                sum += i
+            }
+        } else {
+            sum += i
+        }
+    }
+}
 document.writeln(`<div>Починаємо рахувати з ${min}</div>`);
 document.writeln(`<div>Рахуємо до(включно) ${max}</div>`);
 document.writeln(`<div>Пропускаємо парні числа ${pairNum}</div><br>`);
