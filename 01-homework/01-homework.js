e58b3d9bd211b750dc6cb3959aa44212e9f468f8
unknown <alexandr.ruschak95@gmail.com>
2022-02-21 22:24:53 +0200

    Fixed alert

--- 01-homework/01-homework.js
+++ 01-homework/01-homework.js
@@ -5,30 +5,26 @@ let pairNum;
 
 do {
 	min = +prompt('Від якого числа почнемо додавати?');
+	if ((min !== parseInt(min)) || (Object.is(NaN , min) === true))  alert("Введіть ціле число");
 } while (min % 1 != 0 )
 do {
 	max = +prompt('До якого числа будемо додавати?');
+    if ((max !== parseInt(max)) || (Object.is(NaN , max) === true))  alert("Введіть ціле число");
 } while (max % 1 != 0)
 pairNum = confirm("Пропускаємо парні числа?");
 if (max > min){
 for (i = min; i <= max; i++){
-	if (pairNum === true) {
-            if (i % 2) {
-                sum += i
-            }
-        } else {
-            sum += i
-        }
+    if (pairNum && i % 2 == 0) {
+                continue;
+        } 
+            sum +=i;
     }
 } else{
-	for (i = max; i <= min; i++){
-	if (pairNum === true) {
-            if (i % 2) {
-                sum += i
-            }
-        } else {
-            sum += i
+    for (i = max; i <= min; i++){
+    if (pairNum && i % 2==0) {
+                 continue;
         }
+            sum +=i;
     }
 }
 document.writeln(`<div>Починаємо рахувати з ${min}</div>`);
