7c9ea231a2d880538ce80775cb64ef015c05e719
unknown <alexandr.ruschak95@gmail.com>
2022-05-24 19:12:39 +0300

    Done functions

---
+++ 02-homework/02-homework.js
@@ -0,0 +1,114 @@
+//#1
+function getMaxDigit(num){
+    let big = 0;
+    let arr = num.toString().split('');
+    for(let i=0; i<arr.length; i++){
+       if(arr[i]>big) big = arr[i] 
+    }
+    return big
+ }
+//onsole.log(getMaxDigit(987));
+//№2
+function pow(x, n){
+     let result = x;
+     for (let i=1; i<n;i++){
+         result*=x;
+     }
+     return result;
+ }
+//console.log(pow(2,5))
+ //#3 
+ function formatName(name){
+    return name[0].toUpperCase()+ name.slice(1).toLowerCase()
+ }
+ //console.log(formatName('вЛАД'))
+ //#4 
+ function clearProfit(zp, tax = 19.5){
+     return zp -zp/100 *tax;
+ }
+//console.log(clearProfit(1000));
+//#5 
+ function getRandomNumber(n, m){
+    return Math.floor(Math.random()*(m-n)+n)
+ }
+//console.log(getRandomNumber(1,5))
+//№6
+ function countLetter(letter, word){
+   let count =1;
+   let arr = word.split('')
+   for(let i =0; i<arr.length; i++){
+      if(arr[i]===letter){
+         count++
+      }
+   }
+   return count
+ }
+//console.log(countLetter("а", "Авганістан"))
+ // #7
+ function convertCurrency(sum, curs = 25){
+    let str = sum.toString().toUpperCase();
+    let value = parseInt(sum);
+    if(str.includes('UAH')){
+      return value/curs
+    }else if(str.includes('$')){
+       return value*curs
+    }else{
+      return "Enter current valut 'UAH' or '$'"
+    }
+ }
+//console.log(convertCurrency("2500UH"))
+//#10
+function getRandomPassword(num){
+    let arr = [];
+    for(let i =0; i<num; i++){
+       arr.push(Math.floor(Math.random()*10))
+    }
+    return arr.join('')
+ }
+//console.log(getRandomPassword(8));
+//#11
+ function deleteLetters(letter, word){
+ let res = [];
+ let arr = word.split('')
+ for(let i=0; i<arr.length; i++){
+    if(arr[i]===letter) {
+ continue 
+} else {
+ res.push(arr[i]) 
+}
+ }
+ return res.join('')
+ }
+console.log(deleteLetters('a', 'blablabla'));
+//#12
+function isPalyndrom(str){
+    let arr = deleteLetters(' ', str).toLowerCase()
+    let res = arr.split('').reverse().join('')
+    if(arr===res) {
+ return true 
+} else {
+ return false; 
+}
+ }
+//console.log(isPalyndrom("madam"))
+//№13 
+ function deleteDuplicateLetter(text){
+    return text.toLowerCase().split('').filter((item,pos, self) =>self.indexOf(item)===text.lastIndexOf(item)).join('')
+}
+//console.log(deleteDuplicateLetter("Бісквіт був дуже ніжним"))
+
+document.writeln(`<div><b>Функція №1:</b><p>getMaxDigit(1236)  ==>  ${getMaxDigit(123)}</p></div>`);
+document.writeln(`<div><b>Функція №2:</b><p>Pow(2, 4)==> ${pow(2, 4)}</p></div>`);
+document.writeln(`<div><b>Функція №3:</b><p>formatName('вЛАД')==> ${formatName('влад')}</p></div>`);
+document.writeln(`<div><b>Функція №4:</b><p>clearProfit(1000)==> ${clearProfit(1000)}</p></div>`);
+document.writeln(`<div><b>Функція №5:</b><p>getRandomNumber(1, 10)==> ${getRandomNumber(1, 10)}</p></div>`);
+document.writeln(`<div><b>Функція №6:</b><p>countLetter('a', 'Americaaaaa')==> ${countLetter('a', 'Americaaaaa')}</p></div>`);
+document.writeln(`<div><b>Функція №7:</b><p>convertCurrency('100$')==> ${convertCurrency('100$')}UAH</p></div>`);
+document.writeln(`<p>convertCurrency('2500UAH')==> ${convertCurrency('2500UAH')}$<p>`);
+document.writeln(`<p>convertCurrency('100$')==> ${convertCurrency('100$')}<p>`);
+document.writeln(`<div><b>Функція №8:</b><p>getRandomPassword(4)==> ${getRandomPassword(4)}</p></div>`);
+document.writeln(`<div><b>Функція №9:</b><p>deleteLetters('a', 'blablabla')==> ${deleteLetters('a', 'blablabla')}</p></div>`);
+document.writeln(`<div><b>Функція №10:</b><p>isPalyndrom('madam')==> ${isPalyndrom('madam')}</p></div>`);
+document.writeln(`<p>isPalyndrom('Я несу гусеня')==> ${isPalyndrom('Я несу гусеня')}</p>`);
+document.writeln(`<p>isPalyndrom('кокос')==> ${isPalyndrom('кокос')}</p>`);
+document.writeln(`<div><b>Функція №11:</b><p>deleteDuplicateLetter('Бісквіт був дуже ніжним')==> ${deleteDuplicateLetter('Бісквіт був дуже ніжним')}</p></div>`);
